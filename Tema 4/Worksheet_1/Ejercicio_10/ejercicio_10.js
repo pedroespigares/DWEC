d83094727cd3db374dbffcf3592e0fca7bfe1d5c
Pedro Espigares Asenjo <pedritoesp04@gmail.com>
2022-10-24 18:25:46 +0200

    Ejercicio 5, 9 y 10 (vaya suplicio dios)

---
+++ Tema 4/Worksheet_1/Ejercicio_10/ejercicio_10.js
@@ -0,0 +1,50 @@
+function startDrag(e) {
+  if (e.target) {
+    targ = e.target;
+  } else {
+    targ = e.srcElement;
+  }
+
+  // Calcula las coordenadas actuales al clickar en la imagen
+  offsetX = e.clientX;
+  offsetY = e.clientY;
+
+  // Inicializa la posición de la imagen a 0,0
+  if (!targ.style.left) {
+    targ.style.left = "0px";
+  }
+  if (!targ.style.top) {
+    targ.style.top = "0px";
+  }
+
+  // Pasa la posición a Int
+  coordX = parseInt(targ.style.left);
+  coordY = parseInt(targ.style.top);
+  drag = true;
+
+  // Llama a la función de mover imagen
+  document.onmousemove = dragImg;
+  return false;
+}
+
+function dragImg(e) {
+  // Si la la variable drag está en false (que se ha soltado el click), no realiza la acción.
+  if (!drag) {
+    return false;
+  }
+
+  // Calcula la diferencia de píxeles entre el primer click y cuando se suelta el ratón, moviendo la imagen esa cantidad de píxeles
+  targ.style.left = coordX + e.clientX - offsetX + "px";
+  targ.style.top = coordY + e.clientY - offsetY + "px";
+  return false;
+}
+
+function stopDrag() {
+  // Función para parar de mover la imagen
+  drag = false;
+}
+
+window.onload = () => {
+  document.onmousedown = startDrag;
+  document.onmouseup = stopDrag;
+};
--- Tema 4/Worksheet_1/Ejercicio_5/ejercicio_5.js
+++ Tema 4/Worksheet_1/Ejercicio_5/ejercicio_5.js
@@ -1,18 +1,26 @@
-document.write("<table>")
-for(let i=0;i<=100;i++){
+document.write("<table>");
+for (let i = 0; i <= 100; i++) {
   document.write("<tr>");
-  for(let j=0;j<=100;j++){
-    document.write("<td></td>")
+  for (let j = 0; j <= 100; j++) {
+    document.write("<td></td>");
   }
   document.write("</tr>");
 }
-document.write("</table>")
+document.write("</table>");
 
 window.onload = () => {
   let celdas = document.getElementsByTagName("td");
+  let boton = document.querySelector("button");
+
   for (i = 0; i < celdas.length; i++) {
     celdas[i].onmouseover = cambiaColor;
   }
+
+  boton.addEventListener("click", () => {
+    for (i = 0; i < celdas.length; i++) {
+      celdas[i].style.backgroundColor = "white";
+    }
+  });
 
   function cambiaColor(e) {
     if (e.ctrlKey === true) {
