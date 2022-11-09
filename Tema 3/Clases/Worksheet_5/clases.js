7b7c227acfabf723f84bebe70d4266f488021298
Pedro Espigares <pedritoesp04@gmail.com>
2022-11-09 11:52:44 +0100

    Cambios clases libros

--- Tema 3/Clases/Worksheet_5/clases.js
+++ Tema 3/Clases/Worksheet_5/clases.js
@@ -1,86 +1,54 @@
 class Book {
-  constructor(title, genre, author) {
-    this.title = title;
-    this.genre = genre;
-    this.author = author;
+  constructor(tit, aut, gen) {
+    this.title = tit;
+    this.genre = gen;
+    this.author = aut;
     this.read = false;
-    this.reading = false;
-    this.read_date = undefined;
-  }
-
-  markAsRead() {
-    this.read = true;
-    this.read_date = new Date(Date.now());
-  }
-
-  markAsReading() {
-    this.reading = true;
+    this.readDate = null;
   }
 }
 
 class BookList {
   constructor() {
-    this.readed_books = [];
-    this.not_readed_books = [];
-    this.nextBook = undefined;
-    this.currentBook = undefined;
-    this.lastBook = undefined;
+    this.books = [];
+
+    // Por defecto el libro actual es el primero
+
+    this.currentBook = 0;
   }
 
   add(book) {
-    if (book.read == true) {
-      this.readed_books.push(book);
-    } else {
-      this.not_readed_books.push(book);
-    }
-
-    if (book.reading == true) {
-      this.currentBook = book;
-    }
+    this.books.push(book);
   }
 
   finishCurrentBook() {
-    this.currentBook.markAsRead();
-    this.lastBook = this.currentBook;
-    let index = this.not_readed_books.findIndex(
-      (book) => book === this.currentBook
-    );
-    this.not_readed_books.splice(index, 1);
-    this.readed_books.push(this.lastBook);
-    this.nextBook = this.not_readed_books[0];
-    this.currentBook = this.nextBook;
-    this.nextBook = this.not_readed_books[1];
-    this.currentBook.markAsReading();
+    if (this.currentBook < this.books.length) {
+      // Marcar el libro actual como leído
+
+      this.books[this.currentBook].read = true;
+
+      // Guardar la fecha de lectura (actual)
+
+      this.books[this.currentBook].readDate = new Date();
+      this.currentBook++;
+    }
+  }
+
+  get numberBooksRead() {
+    // Devolver el número de libros leídos
+
+    return this.books.filter((book) => book.read).length;
+  }
+
+  get numberBooksNotReadYet() {
+    // Devolver el número de libros no leídos
+
+    return this.books.length - this.numberBooksRead;
+  }
+
+  get totalBooks() {
+    // Devolver el número total de libros
+
+    return this.books.length;
   }
 }
-
-let libro_1 = new Book("Chainsaw Man", "Acción", "Tatsuki Fujimoto");
-
-libro_1.markAsRead();
-
-let libro_2 = new Book("Don Quijote", "Clásico", "Miguel de Cervantes");
-
-libro_2.markAsRead();
-
-let libro_3 = new Book("Marina", "Ficción", "Juan Luis Pérez");
-
-let libro_4 = new Book("Romeo y Julieta", "Romance", "William Shakespear");
-
-let libro_5 = new Book("Jujutsu Kaisen", "Acción", "Carlos Puerro");
-
-libro_5.markAsReading();
-
-let libreria = new BookList();
-
-libreria.add(libro_1);
-libreria.add(libro_2);
-libreria.add(libro_3);
-libreria.add(libro_4);
-libreria.add(libro_5);
-libreria.finishCurrentBook();
-
-console.log("Libros leídos: ", libreria.readed_books);
-console.log("Libros no leídos: ", libreria.not_readed_books);
-console.log("Libro actual: ", libreria.currentBook);
-console.log("Último libro: ", libreria.lastBook);
-console.log("Próximo libro: ", libreria.nextBook);
