00fc78e151c5731644d86b4bef28de15cf506d25
Pedro Espigares <pedritoesp04@gmail.com>
2023-01-10 12:45:16 +0100

    Reminder List con Vue

--- VueJS/blog.js
+++ VueJS/blog.js
@@ -10,13 +10,21 @@ const { createApp } = Vue
                 codigo: "1",
                 titulo: "Londres",
                 texto: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sagittis erat nibh, ac hendrerit ex gravida quis. Vivamus viverra dui sed bibendum porttitor. Morbi gravida est sed erat fermentum, eget pharetra enim maximus. Phasellus in sapien rhoncus, euismod nisi quis, commodo leo. Morbi convallis, mauris at pulvinar volutpat, turpis turpis accumsan purus, sed suscipit purus ante dapibus odio. Praesent in lorem consectetur felis tincidunt porttitor in eget nunc. Cras id tellus in diam tempor volutpat. Maecenas hendrerit auctor semper.",
-                url: "http://google.com"
+                url: "http://google.com",
+                ubicacion: {
+                  ciudad: "Londres",
+                  pais: "Inglaterra"
+                }
             },
             {
                 codigo: "2",
                 titulo: "Paris",
                 texto: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sagittis erat nibh, ac hendrerit ex gravida quis. Vivamus viverra dui sed bibendum porttitor. Morbi gravida est sed erat fermentum, eget pharetra enim maximus. Phasellus in sapien rhoncus, euismod nisi quis, commodo leo. Morbi convallis, mauris at pulvinar volutpat, turpis turpis accumsan purus, sed suscipit purus ante dapibus odio. Praesent in lorem consectetur felis tincidunt porttitor in eget nunc. Cras id tellus in diam tempor volutpat. Maecenas hendrerit auctor semper.",
-                url: "http://gmail.com"
+                url: "http://gmail.com",
+                ubicacion: {
+                  ciudad: "Paris",
+                  pais: "Francia"
+                }
             }
         ]
       }
@@ -27,6 +35,11 @@ const { createApp } = Vue
       },
       cambiarVisibilidad(){
         return this.verEntradas = !this.verEntradas;
+      }
+    },
+    computed: {
+      entradasDeInglaterra(){
+        return this.entradas.filter((entrada) => entrada.ubicacion.pais === "Inglaterra").length;
       }
     }
   }).mount('#app')
